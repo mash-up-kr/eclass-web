1cbd3892d7146a4497d65812018f54a2672176c1
mango906 <minkb01@gmail.com>
2021-10-01 21:59:10 +0900

    :lipstick: 웹뷰에서 필요한 기본적인 스타일 세팅들 추가

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
   plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-config-rational-order/plugin'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+      },
+    ],
+    'declaration-empty-line-before': [
+      'always',
+      {
+        ignore: ['after-comment', 'after-declaration', 'first-nested', 'inside-single-line-block'],
+      },
+    ],
+  },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import 'styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
