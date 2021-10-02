ec44e5586122d41d0a72f29777bad40cc1f01ddf
mango906 <minkb01@gmail.com>
2021-10-03 00:26:18 +0900

    :sparkles: FixedHeader 월 선택 아이콘 추가

--- src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
+++ src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as CalendarIcon } from 'assets/ic_calendar.svg';
+import { ReactComponent as CaretDownIcon } from 'assets/ic_caret_down.svg';
 
 import style from './FixedHeader.module.scss';
 
@@ -6,7 +7,11 @@ const FixedHeader = () => {
   return (
     <header className={style['fixed-header']}>
       <p className={style['fixed-header__logo']}>로고</p>
-      <p className={style['fixed-header__month']}>10월</p>
+      <div className={style['fixed-header__month-picker']}>
+        <p className={style['fixed-header__month']}>10월</p>
+        <CaretDownIcon className={style['fixed-header__caret-icon']} />
+      </div>
+
       <div className={style['fixed-header__calendar']}>
         <CalendarIcon className={style['fixed-header__calendar-icon']} />
       </div>
