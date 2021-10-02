cdf5d51e512c751a52931ecfebd9fb1d0f009f45
mango906 <minkb01@gmail.com>
2021-10-03 03:58:28 +0900

    :recycle: 기존에 svg 쓰는 부분들 SVGButton으로 대체

--- src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
+++ src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
@@ -3,17 +3,19 @@ import { ReactComponent as CaretDownIcon } from 'assets/ic_caret_down.svg';
 
 import style from './FixedHeader.module.scss';
 
+import SVGButton from 'components/SVGButton';
+
 const FixedHeader = () => {
   return (
     <header className={style['fixed-header']}>
       <p className={style['fixed-header__logo']}>로고</p>
       <div className={style['fixed-header__month-picker']}>
         <p className={style['fixed-header__month']}>10월</p>
-        <CaretDownIcon className={style['fixed-header__caret-icon']} />
+        <SVGButton icon={<CaretDownIcon />} className={style['fixed-header__caret-icon']} />
       </div>
 
       <div className={style['fixed-header__calendar']}>
-        <CalendarIcon className={style['fixed-header__calendar-icon']} />
+        <SVGButton icon={<CalendarIcon />} className={style['fixed-header__calendar-icon']} />
       </div>
     </header>
   );
