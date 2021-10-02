af8617cbe540d84381040600083e14a0e3deccb4
mango906 <minkb01@gmail.com>
2021-10-03 07:22:57 +0900

    :sparkles: 다이어리 카드 눌렀을 떄 상세페이지로 이동하도록 설정

--- src/pages/MonthlyDiaryPage/index.tsx
+++ src/pages/MonthlyDiaryPage/index.tsx
@@ -2,6 +2,8 @@ import style from './MonthlyDiaryPage.module.scss';
 import DiaryCard from './components/DiaryCard';
 import FixedHeader from './components/FixedHeader';
 
+import { Link } from 'react-router-dom';
+
 const MonthlyDiaryPage = () => {
   return (
     <>
@@ -9,22 +11,34 @@ const MonthlyDiaryPage = () => {
       <div className={style['monthly-diary']}>
         <ul className={style['monthly-diary__list']}>
           <li className={style['monthly-diary__item']}>
-            <DiaryCard />
+            <Link to="/diary-detail">
+              <DiaryCard />
+            </Link>
           </li>
           <li className={style['monthly-diary__item']}>
-            <DiaryCard />
+            <Link to="/diary-detail">
+              <DiaryCard />
+            </Link>
           </li>
           <li className={style['monthly-diary__item']}>
-            <DiaryCard />
+            <Link to="/diary-detail">
+              <DiaryCard />
+            </Link>
           </li>
           <li className={style['monthly-diary__item']}>
-            <DiaryCard />
+            <Link to="/diary-detail">
+              <DiaryCard />
+            </Link>
           </li>
           <li className={style['monthly-diary__item']}>
-            <DiaryCard />
+            <Link to="/diary-detail">
+              <DiaryCard />
+            </Link>
           </li>
           <li className={style['monthly-diary__item']}>
-            <DiaryCard />
+            <Link to="/diary-detail">
+              <DiaryCard />
+            </Link>
           </li>
         </ul>
       </div>
