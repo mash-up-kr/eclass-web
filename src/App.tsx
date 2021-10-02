9b36102bffbc4be15de05a84cf4fbd8c9152ad0c
Baek2back <dumbuk12@naver.com>
2021-10-03 06:04:24 +0900

    ✨ 상세 다이어리 페이지 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 const LazyMonthlyDiaryPage = lazy(() => import(/* webpackChunkName: "MonthlyDiaryPage" */ 'pages/MonthlyDiaryPage'));
+const LazyDiaryDetailPage = lazy(() => import(/* webpackChunkName: "DiaryDetailPage" */ 'pages/DiaryDetailPage'));
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
             <Redirect to="/monthly-diary" />
           </Route>
           <Route exact path="/monthly-diary" component={LazyMonthlyDiaryPage} />
+          <Route exact path="/diary-detail" component={LazyDiaryDetailPage} />
         </Switch>
       </BrowserRouter>
     </Suspense>
