cbc38005e3138561e03dddf24f5b971b2df0e8af
mango906 <minkb01@gmail.com>
2021-10-02 14:44:56 +0900

    :sparkles: MainPage 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router';
 
-import React from 'react';
+// Sample Code
+const LazyMainPage = lazy(() => import(/* webpackChunkName: "Main" */ 'pages/MainPage'));
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={null}>
+      <Switch>
+        <Route exact path="/" component={LazyMainPage} />
+      </Switch>
+    </Suspense>
   );
-}
+};
 
 export default App;
