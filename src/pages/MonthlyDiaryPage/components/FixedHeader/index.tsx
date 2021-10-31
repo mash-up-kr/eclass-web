dc62245b14db7902d5c4ef6f3aa4d0874de9533f
mango906 <minkb01@gmail.com>
2021-10-31 22:57:51 +0900

    :lipstick: 다이어리 목록 디자인 변경사항에 맞춰 변경

--- src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
+++ src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
@@ -1,6 +1,4 @@
-import { ReactComponent as CalendarIcon } from 'assets/ic_calendar.svg';
 import { ReactComponent as CaretDownIcon } from 'assets/ic_caret_down.svg';
-import { ReactComponent as LogoIcon } from 'assets/ic_logo.svg';
 
 import styles from './FixedHeader.module.scss';
 
@@ -13,14 +11,9 @@ const cx = classNames.bind(styles);
 const FixedHeader = React.memo(() => {
   return (
     <header className={cx('fixed-header')}>
-      <SVGButton icon={<LogoIcon />} className={cx('fixed-header__logo')} />
       <div className={cx('fixed-header__month-picker')}>
         <p className={cx('fixed-header__month')}>10월</p>
         <SVGButton icon={<CaretDownIcon />} className={cx('fixed-header__caret-icon')} />
-      </div>
-
-      <div className={cx('fixed-header__calendar')}>
-        <SVGButton icon={<CalendarIcon />} className={cx('fixed-header__calendar-icon')} />
       </div>
     </header>
   );
