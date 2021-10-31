26dc52217dc762a053ca7a7b12f1f40da16aa8d5
Baek2back <dumbuk12@naver.com>
2021-11-01 00:21:09 +0900

    :arrow_up: Swiper 7.2.0로 업그레이드

--- src/pages/DiaryDetailPage/components/Carousel/index.tsx
+++ src/pages/DiaryDetailPage/components/Carousel/index.tsx
@@ -3,19 +3,17 @@ import DUMMY_DIARY_DOG_IMAGE from 'assets/img_dummy_diary_dog.png';
 import styles from './Carousel.module.scss';
 
 import classNames from 'classnames/bind';
-import SwiperCore, { Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 
 import 'swiper/swiper.scss';
-import 'swiper/components/pagination/pagination.scss';
+import 'swiper/modules/pagination/pagination.scss';
 
 const cx = classNames.bind(styles);
 
 const Carousel = () => {
-  SwiperCore.use([Pagination]);
-
   return (
-    <Swiper className={cx('carousel')} slidesPerView={1} pagination={{ clickable: true }}>
+    <Swiper modules={[Pagination]} className={cx('carousel')} slidesPerView={1} pagination={{ clickable: true }}>
       <SwiperSlide>
         <img className={cx('carousel__item')} src={DUMMY_DIARY_DOG_IMAGE} alt="" />
       </SwiperSlide>
