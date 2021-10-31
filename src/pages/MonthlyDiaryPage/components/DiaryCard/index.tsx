edff9f435543e48a859bfb0179ebe92c83491ba7
mango906 <minkb01@gmail.com>
2021-10-31 22:41:01 +0900

    :sparkles: 다이어리 데이터 api로 가져와서 사용하도록 변경

--- src/pages/MonthlyDiaryPage/components/DiaryCard/index.tsx
+++ src/pages/MonthlyDiaryPage/components/DiaryCard/index.tsx
@@ -1,21 +1,24 @@
 import { ReactComponent as GreenDotIcon } from 'assets/ic_green_dot.svg';
 import { ReactComponent as MoreHorizontalIcon } from 'assets/ic_more_horizontal.svg';
-import DUMMY_DOG_THUMBNAIL from 'assets/img_dummy_dog.png';
 
 import styles from './DiaryCard.module.scss';
 
 import classNames from 'classnames/bind';
 import SVGButton from 'components/SVGButton';
 import React, { useCallback } from 'react';
+import { Diary } from 'models/Diary';
 
 const cx = classNames.bind(styles);
 
 interface DiaryCardProps {
+  diary: Diary;
   className?: string;
   onOpenModal?: () => void;
 }
 
-const DiaryCard = React.memo(({ className, onOpenModal }: DiaryCardProps) => {
+const DiaryCard = React.memo(({ diary, className, onOpenModal }: DiaryCardProps) => {
+  const firstThumbnail = diary.pictureSubmitRequestList[0].imageUrl;
+
   const handleOpenModal = useCallback(
     (event: React.MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
@@ -34,12 +37,10 @@ const DiaryCard = React.memo(({ className, onOpenModal }: DiaryCardProps) => {
       </header>
       <div className={cx('diary-card__article')}>
         <div className={cx('diary-card__summary')}>
-          <p className={cx('diary-card__content')}>
-            오늘은 보리랑 동두천에 산책을 갔는데 아니 글쎄 옆집 철수를 만나가지구 넘 반갑더라
-          </p>
+          <p className={cx('diary-card__content')}>{diary.content}</p>
           <p className={cx('diary-card__extra-content')}>집사 &#183; 댓글12</p>
         </div>
-        <img className={cx('diary-card__thumbnail')} src={DUMMY_DOG_THUMBNAIL} alt="diary-thumbnail" />
+        <img className={cx('diary-card__thumbnail')} src={firstThumbnail} alt="diary-thumbnail" />
       </div>
     </div>
   );
