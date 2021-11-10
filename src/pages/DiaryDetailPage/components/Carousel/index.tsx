26e56566b29da447ba0db4327ee62f7c49cf198d
mango906 <minkb01@gmail.com>
2021-11-10 23:28:04 +0900

    :bug: 린트 에러 수정

--- src/pages/DiaryDetailPage/components/Carousel/index.tsx
+++ src/pages/DiaryDetailPage/components/Carousel/index.tsx
@@ -1,3 +1,6 @@
+import 'swiper/swiper.scss';
+import 'swiper/modules/pagination/pagination.scss';
+
 import DUMMY_DIARY_DOG_IMAGE from 'assets/img_dummy_diary_dog.png';
 
 import styles from './Carousel.module.scss';
@@ -5,9 +8,6 @@ import styles from './Carousel.module.scss';
 import classNames from 'classnames/bind';
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
-
-import 'swiper/swiper.scss';
-import 'swiper/modules/pagination/pagination.scss';
 
 const cx = classNames.bind(styles);
 
