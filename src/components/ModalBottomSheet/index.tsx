9d354a50bca16ab16cecc8ffc3bf6f905fb051df
mango906 <minkb01@gmail.com>
2021-11-11 21:01:51 +0900

    :recycle: Overlay 열렸을 때 window.scrollY 안되도록 변경

--- src/components/ModalBottomSheet/index.tsx
+++ src/components/ModalBottomSheet/index.tsx
@@ -2,7 +2,6 @@ import styles from './ModalBottomSheet.module.scss';
 
 import classNames from 'classnames/bind';
 import Overlay from 'components/Overlay';
-import { useEffect } from 'react';
 import { useSwipeable } from 'react-swipeable';
 
 interface ModalBottomSheetProps {
@@ -17,14 +16,6 @@ const ModalBottomSheet = ({ isOpened, children, onClose }: ModalBottomSheetProps
   const swipeableHandlers = useSwipeable({
     onSwipedDown: onClose,
   });
-
-  useEffect(() => {
-    if (isOpened) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'visible';
-    }
-  }, [isOpened]);
 
   return (
     <Overlay align="bottom" isOpened={isOpened} onClick={onClose}>
