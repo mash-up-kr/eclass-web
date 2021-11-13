02e0a04171236fb482a1d320be788621c55ca42b
mango906 <minkb01@gmail.com>
2021-11-13 17:46:55 +0900

    :recycle: useToast dependency array 타입 변경

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -2,15 +2,16 @@ import { toastTextAtom } from 'components/Toast';
 import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 
+type DependencyArray = unknown[];
+
 interface UseToastParams {
   conditions: boolean[];
   message: string;
-  dependencies: unknown[];
 }
 
 const TOAST_DURATION_TIME = 2000;
 
-export const useToast = ({ conditions, message, dependencies }: UseToastParams): void => {
+export const useToast = ({ conditions, message }: UseToastParams, dependencies?: DependencyArray): void => {
   const setToastText = useSetRecoilState(toastTextAtom);
 
   useEffect(() => {
