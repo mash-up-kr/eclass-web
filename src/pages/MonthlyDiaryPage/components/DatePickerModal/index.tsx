0c8b2eae4afb2fca55bf68cb2154ff3517639d22
mango906 <minkb01@gmail.com>
2021-11-14 00:58:59 +0900

    :sparkles: 날짜 선택하는 모달 추가 & 모달 방식 변경

---
+++ src/pages/MonthlyDiaryPage/components/DatePickerModal/index.tsx
@@ -0,0 +1,91 @@
+/* eslint-disable array-callback-return */
+import { ReactComponent as CaretLeft } from 'assets/ic_caret_left.svg';
+import { ReactComponent as CaretRight } from 'assets/ic_caret_right.svg';
+
+import styles from './DatePickerModal.module.scss';
+
+import classNames from 'classnames/bind';
+import Overlay from 'components/Overlay';
+import SVGButton from 'components/SVGButton';
+import dayjs from 'dayjs';
+import { useCallback, useState } from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+interface DatePickerModalProps {
+  isOpened: boolean;
+  onClose: () => void;
+}
+
+const cx = classNames.bind(styles);
+
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const currentYear = dayjs().year();
+const currentMonth = dayjs().month();
+
+export const selectedDateAtom = atom({
+  key: 'selectedDateAtom',
+  default: {
+    year: currentYear,
+    month: currentMonth,
+  },
+});
+
+const DatePickerModal = ({ isOpened, onClose }: DatePickerModalProps) => {
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [selectedDate, setSelectedDate] = useRecoilState(selectedDateAtom);
+
+  const { year: selectedDateYear, month: selectedDateMonth } = selectedDate;
+
+  const handlePrevYear = useCallback(() => {
+    setSelectedYear(selectedYear - 1);
+  }, [selectedYear]);
+
+  const handleNextYear = useCallback(() => {
+    setSelectedYear(selectedYear + 1);
+  }, [selectedYear]);
+
+  const handleSelectMonth = useCallback(
+    (month: number) => () => {
+      setSelectedDate({
+        year: selectedYear,
+        month,
+      });
+
+      onClose();
+    },
+    [onClose, selectedYear, setSelectedDate]
+  );
+
+  return (
+    <Overlay align="center" isOpened={isOpened}>
+      <div className={cx('modal-container')}>
+        <div className={cx('modal-container__header')}>
+          <SVGButton icon={<CaretLeft />} onClick={handlePrevYear} />
+          <p className={cx('modal-container__year')}>{selectedYear}</p>
+          <SVGButton icon={<CaretRight />} onClick={handleNextYear} />
+        </div>
+
+        <div className={cx('modal-container__months')}>
+          {months.map((month) => {
+            const isSelectedItem = selectedDateYear === selectedYear && selectedDateMonth === month;
+
+            return (
+              <div
+                className={cx('modal-container__months-item', {
+                  'modal-container__months-item--selected': isSelectedItem,
+                })}
+                key={month}
+                onClick={handleSelectMonth(month)}
+              >
+                {`${month}월`}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Overlay>
+  );
+};
+
+export default DatePickerModal;
--- src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
+++ src/pages/MonthlyDiaryPage/components/FixedHeader/index.tsx
@@ -1,18 +1,26 @@
 import { ReactComponent as CaretDownIcon } from 'assets/ic_caret_down.svg';
 
+import { selectedDateAtom } from '../DatePickerModal';
+
 import styles from './FixedHeader.module.scss';
 
 import classNames from 'classnames/bind';
 import SVGButton from 'components/SVGButton';
+import { useModal } from 'hooks/useModal';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 
 const cx = classNames.bind(styles);
 
 const FixedHeader = React.memo(() => {
+  const { handleOpenModal } = useModal();
+
+  const { month } = useRecoilValue(selectedDateAtom);
+
   return (
     <header className={cx('fixed-header')}>
-      <div className={cx('fixed-header__month-picker')}>
-        <p className={cx('fixed-header__month')}>10월</p>
+      <div className={cx('fixed-header__month-picker')} onClick={handleOpenModal('DATE_PICKER')}>
+        <p className={cx('fixed-header__month')}>{month}월</p>
         <SVGButton icon={<CaretDownIcon />} className={cx('fixed-header__caret-icon')} />
       </div>
     </header>
