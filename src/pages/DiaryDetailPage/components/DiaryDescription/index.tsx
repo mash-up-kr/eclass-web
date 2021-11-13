6b936991546eb365ec55b48b3298e59d2c0fbbce
김성백 <dumbuk12@naver.com>
2021-11-14 05:56:01 +0900

    ✨ 상세 다이어리 페이지 데이터 API 연동

--- src/pages/DiaryDetailPage/components/DiaryDescription/index.tsx
+++ src/pages/DiaryDetailPage/components/DiaryDescription/index.tsx
@@ -3,17 +3,21 @@ import DiaryDescriptionHeader from '../DiaryDescriptionHeader';
 import styles from './DiaryDescription.module.scss';
 
 import classNames from 'classnames/bind';
+import { Badge } from 'models/Diary';
 
 const cx = classNames.bind(styles);
 
-const DiaryDescription = () => {
+interface DiaryDescriptionProps {
+  content: string;
+  createdAt: string;
+  badgeResponseDto: Badge;
+}
+
+const DiaryDescription = ({ content, createdAt, badgeResponseDto }: DiaryDescriptionProps) => {
   return (
     <div className={cx('diary-description')}>
-      <DiaryDescriptionHeader />
-      <p className={cx('diary-description__content')}>
-        오늘은 보리랑 동두천에 산책을 갔는데 아니 글쎄 옆집 철수를 만나가지구 넘 반가웠다 음하하하하하아 오늘은 보리랑
-        동두천에 산책을 갔는데
-      </p>
+      <DiaryDescriptionHeader createdAt={createdAt} badgeResponseDto={badgeResponseDto} />
+      <p className={cx('diary-description__content')}>{content}</p>
     </div>
   );
 };
--- src/pages/DiaryDetailPage/components/DiaryDescriptionHeader/index.tsx
+++ src/pages/DiaryDetailPage/components/DiaryDescriptionHeader/index.tsx
@@ -1,24 +1,29 @@
-import { ReactComponent as GreenDotIcon } from 'assets/ic_green_dot.svg';
-
 import styles from './DiaryDescriptionHeader.module.scss';
 
 import classNames from 'classnames/bind';
+import { Badge } from 'models/Diary';
+import { formatDate } from 'utils/date';
 
 const cx = classNames.bind(styles);
 
-const DiaryDescriptionHeader = () => {
+interface DiaryDescriptionHeaderProps {
+  createdAt: string;
+  badgeResponseDto: Badge;
+}
+
+const DiaryDescriptionHeader = ({ createdAt, badgeResponseDto }: DiaryDescriptionHeaderProps) => {
   return (
     <div className={cx('diary-description-header')}>
       <div className={cx('diary-description-header__header')}>
         <div className={cx('diary-description-header__status')}>
-          <GreenDotIcon width="24" height="24" />
+          <img width={24} height={24} src={badgeResponseDto.imageUrl} alt={badgeResponseDto.name} />
         </div>
-        <h1 className={cx('diary-description-header__title')}>자두자두 졸려요...</h1>
+        <h1 className={cx('diary-description-header__title')}>{badgeResponseDto.name}</h1>
       </div>
       <div className={cx('diary-description-header__sub-header')}>
         <span className={cx('diary-description-header__author')}>엄마</span>
-        <time className={cx('diary-description-header__date')} dateTime="2021/08/14">
-          2021. 08. 14
+        <time className={cx('diary-description-header__date')} dateTime={createdAt}>
+          {formatDate(createdAt, 'YYYY. MM. DD')}
         </time>
       </div>
     </div>
