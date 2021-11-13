e057fbda91853d55f21adf5782a9707c870f988b
김성백 <dumbuk12@naver.com>
2021-11-14 05:56:01 +0900

    ✨ 상세 다이어리 페이지 데이터 API 연동

--- src/pages/DiaryDetailPage/components/Carousel/index.tsx
+++ src/pages/DiaryDetailPage/components/Carousel/index.tsx
@@ -2,27 +2,50 @@ import 'swiper/swiper.scss';
 import 'swiper/modules/pagination/pagination.scss';
 
 import DUMMY_DIARY_DOG_IMAGE from 'assets/img_dummy_diary_dog.png';
+import DUMMY_STICKER from 'assets/sticker/ic_socks.svg';
 
 import styles from './Carousel.module.scss';
 
 import classNames from 'classnames/bind';
+import { DiaryPicture } from 'models/Diary';
+import { useRef, useEffect } from 'react';
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 
 const cx = classNames.bind(styles);
 
-const Carousel = () => {
+interface CarouselProps {
+  pictureSubmitRequestList: DiaryPicture[];
+}
+
+const Carousel = ({ pictureSubmitRequestList }: CarouselProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const ctx = canvasRef.current.getContext('2d');
+    if (!ctx) return;
+
+    pictureSubmitRequestList[0].attachedStickerDtoList.forEach((sticker) => {
+      const image = new Image();
+      image.src = DUMMY_STICKER;
+      image.onload = () => {
+        ctx.drawImage(image, sticker.stickerX, sticker.stickerY, 50, 60);
+      };
+    });
+  }, [pictureSubmitRequestList]);
+
   return (
     <Swiper modules={[Pagination]} className={cx('carousel')} slidesPerView={1} pagination={{ clickable: true }}>
-      <SwiperSlide>
-        <canvas className={cx('carousel__item')} style={{ backgroundImage: `url(${DUMMY_DIARY_DOG_IMAGE})` }} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <canvas className={cx('carousel__item')} style={{ backgroundImage: `url(${DUMMY_DIARY_DOG_IMAGE})` }} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <canvas className={cx('carousel__item')} style={{ backgroundImage: `url(${DUMMY_DIARY_DOG_IMAGE})` }} />
-      </SwiperSlide>
+      {pictureSubmitRequestList.map((diaryPicture) => (
+        <SwiperSlide key={diaryPicture.diaryPictureId}>
+          <canvas
+            ref={canvasRef}
+            className={cx('carousel__item')}
+            style={{ backgroundImage: `url(${DUMMY_DIARY_DOG_IMAGE})` }}
+          />
+        </SwiperSlide>
+      ))}
       <button type="button">스티커 추가</button>
     </Swiper>
   );
--- src/pages/DiaryDetailPage/index.tsx
+++ src/pages/DiaryDetailPage/index.tsx
@@ -1,17 +1,26 @@
 import styles from './DiaryDetailPage.module.scss';
 import Carousel from './components/Carousel';
 import DiaryDescription from './components/DiaryDescription';
+import useDiaryDetail from './useDiaryDetail';
 
 import classNames from 'classnames/bind';
+import { useParams } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const DiaryDetailPage = () => {
+  const { diaryId } = useParams<{ diaryId: string }>();
+  const { diaryDetail } = useDiaryDetail(diaryId);
+
+  if (!diaryDetail) return null;
+
+  const { pictureSubmitRequestList, createdAt, badgeResponseDto, content } = diaryDetail;
+
   return (
     <>
       <main className={cx('diary-detail-page__main')}>
-        <Carousel />
-        <DiaryDescription />
+        <Carousel pictureSubmitRequestList={pictureSubmitRequestList} />
+        <DiaryDescription createdAt={createdAt} content={content} badgeResponseDto={badgeResponseDto} />
       </main>
       <footer className={cx('diary-detail-page__footer')} />
     </>
